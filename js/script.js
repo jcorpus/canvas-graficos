f4935c506bdf59beddcb6ed7bdb0219a2a25fcdf
jcorpus <doombakuryo@gmail.com>
2016-11-11 21:43:58 -0500

    cambiando

--- js/script.js
+++ js/script.js
@@ -4,16 +4,12 @@ $( window ).load(function() {
     });
 */
 
-
-///
 $(document).ready(function(){
-
-
 
 	var canvas = document.getElementById("dibujo");//$("#dibujo").val();
 	//contexto = ctx
-	if (canvas.getContext) {
-	var ctx = canvas.getContext("2d");
+	var ctx= canvas.getContext("2d");
+
 	//#0095C2
 	ctx.fillStyle = "rgb(0,149,194)";
 	ctx.fillRect(100,100,50,50);
@@ -35,10 +31,28 @@ $(document).ready(function(){
 	ctx.lineTo(800,0);
 	ctx.lineTo(350,160);
 	ctx.fill(); //cierra el camiino
+	ctx.closePath();
+  ///texto con gradiente
+	ctx.beginPath();
+	ctx.font = "70px Sans serif";
+	var gradiente = ctx.createLinearGradient(0,0,700,0);
+	gradiente.addColorStop("0","magenta");
+	gradiente.addColorStop("0.5","blue");
+	gradiente.addColorStop("1.0","red");
+	ctx.fillStyle = gradiente;
+
+	ctx.fillText("Probando",300,430);
+	ctx.closePath();
+
+	ctx.beginPath();
+	ctx.arc(400,250,100,0,2*Math.PI);
+	ctx.fillStyle = gradiente;
+	ctx.fill();
 
 
 
-	}
+
+
 
 
 });
